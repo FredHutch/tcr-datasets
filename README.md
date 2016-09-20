cf7ed2706c153269d011db3c2aab3c8304e811f6
Michael Gutteridge <mrg@fhcrc.org>
2016-09-20 15:07:27 -0700

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# pbradley-tcr
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'pbradley-tcr'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures pbradley-tcr'
+long_description 'Installs/Configures pbradley-tcr'
+version '0.1.0'
