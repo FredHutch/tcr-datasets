1727df524c1cf55b9d508945c5ede14265dfcf07
Michael Gutteridge <mrg@fhcrc.org>
2016-09-21 08:34:51 -0700

    updated comment

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,8 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 #
 
-# This deploys the deploy key from an encrypted databag
+# This deploys the deploy key from an encrypted databag and configures
+# an ssh-wrapper for the deploy step.
 
 include_recipe 'chef-vault::default'
 deploy_key = chef_vault_item('tcr-datasets', 'deploy_key')
@@ -28,3 +29,5 @@ file '/tmp/tcr-datasets/ssh_wrap.sh' do
           'ssh -o "StrictHostKeyChecking=no" '\
           '-i "/tmp/tcr-datasets/.ssh/id" $1'
 end
+
+
