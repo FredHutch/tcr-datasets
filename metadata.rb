eb55498718e4c8dc6513892e4d9fdc07f98fb2ce
Michael Gutteridge <mrg@fhcrc.org>
2016-09-22 17:37:22 -0700

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures tcr-datasets'
 long_description 'Installs/Configures tcr-datasets'
-version '0.2.0'
+version '0.2.1'
 issues_url 'https://github.com/FredHutch/tcr-datasets/issues'
 source_url 'https://github.com/FredHutch/tcr-datasets'
 
