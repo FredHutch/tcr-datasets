d099751237ca93175473b0de5b5e6d4d9c951328
Michael Gutteridge <mrg@fhcrc.org>
2016-11-28 11:39:12 -0800

    Fix redirect to https from http
    Update template and nginx config to get http->https redirects working

--- attributes/nginx_config.rb
+++ attributes/nginx_config.rb
@@ -9,18 +9,3 @@ node.default['tcr_datasets']['nginx_config'] = {
 }
 
 node.override['nginx']['default_site_enabled'] = false
-node.override['nginx']['default']['modules'] = ['socketproxy']
-node.override['nginx']['port'] = '80'
-
-node.override['nginx']['socketproxy']['root'] = '/var/www/apps'
-
-node.override['nginx']['socketproxy']['default_app'] = 'tcr_datasets'
-node.override['nginx']['socketproxy']['apps'] = {
-  'tcr_datasets' => {
-    'subdir' => 'tcr_datasets',
-    'socket' => {
-      'type' => 'tcp',
-      'port' => '8443'
-    }
-  }
-}
